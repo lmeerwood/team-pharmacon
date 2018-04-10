abd1faa76a315d7384b995832f5de4e2f43a644f
Leonard Meerwood <leonard.meerwood@outlook.com>
2018-04-10 17:52:01 +1000

    Added API to add information to table

--- code/server/app.js
+++ code/server/app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql =require('mysql');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tableRouter = require('./routes/table');
 
 var app = express();
 
@@ -20,22 +21,27 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/tableData', tableRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.use(function (req, res, next) {
+    console.warn('Time: %d', Date.now());
+    next();
 });
 
 module.exports = app;
