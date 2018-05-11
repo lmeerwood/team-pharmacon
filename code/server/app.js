d48d19de0b89bb47fbef1a8448d535007ca2e254
Leonard Meerwood <leonard.meerwood@outlook.com>
2018-05-11 12:28:49 +1000

    Adjusted server code to use Standard. Server lints code before execution on 'dev'

--- code/server/app.js
+++ code/server/app.js
@@ -1,37 +1,38 @@
-var express = require('express');
-var path = require('path');
-var mysql =require('mysql');
+var express = require('express')
+var path = require('path')
+// var mysql = require('mysql')
 
-var app = express();
+var app = express()
 
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+// MySQL setup
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+app.use(function (req, res, next) {
+  // render the error page
+  res.status(404)
+  res.render('not found')
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 app.use(function (req, res, next) {
-    console.warn('Time: %d', Date.now());
-    next();
-});
+  console.warn('Time: %d', Date.now())
+  next()
+})
 
-module.exports = app;
+module.exports = app
