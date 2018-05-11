3333800aafd9e6d90d0a2e8b99b2eba6c68de992
Leonard Meerwood <leonard.meerwood@outlook.com>
2018-05-11 17:18:13 +1000

    Added demo API call.

--- code/server/app.js
+++ code/server/app.js
@@ -1,22 +1,51 @@
 var express = require('express')
-var path = require('path')
-// var mysql = require('mysql')
+// var path = require('path')
+var mysql = require('mysql')
+var bodyParser = require('body-parser')
 
 var app = express()
 
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+// Set up Body Parser
+app.use(bodyParser.json()) // Support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // Support encoded bodies
+
+var queryRoute = require('./routes/query')
+
+// app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // MySQL setup
+app.use(function (req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'petdatabase'
+  })
+  res.locals.connection.connect(function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Successfully connected to database')
+    }
+  })
+  next()
+})
+
+// Define Query URL
+app.use('/api/v1/query', queryRoute)
+
+// View Engine
+app.set('view engine', 'pug')
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   // render the error page
   res.status(404)
-  res.render('not found')
+  res.send('not found')
 })
 
 // error handler
@@ -26,8 +55,8 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  res.status(500)
+  res.send('error: ' + err.stack)
 })
 
 app.use(function (req, res, next) {
