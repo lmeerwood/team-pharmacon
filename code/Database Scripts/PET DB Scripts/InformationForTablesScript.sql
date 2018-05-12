025eb99f82f4ea01c2a908146a4f721ae244cde7
Jette McKellar <jette.mckellar@yahoo.com>
2018-05-12 20:43:10 +1000

    Final update to PET Database and the data for tables script. Tables that have data added in this script are: errorType, severity, worker, error, medication (18 different types), patient, and physician. Patient & physician have new primary keys to enable a patient to be entered multiple times as they may present as three different types (outpatient, inpatient, discharge) and physicians to make different comments on individual errors.
    Signed-off-by: Jette McKellar <jette.mckellar@yahoo.com>

--- code/Database Scripts/PET DB Scripts/InformationForTablesScript.sql
+++ code/Database Scripts/PET DB Scripts/InformationForTablesScript.sql
@@ -17,7 +17,7 @@ INSERT INTO `petdatabase`.`errortype` (`idErrorType`, `errorType`) VALUES (10, '
 COMMIT;
 
 -- -----------------------------------------------------
--- Data for table `petdatabase`.`severity` Minor, Low, Moderate-Low, Moderate, Moderate-Severe, Severe
+-- Data for table `petdatabase`.`severity`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `petdatabase`;
@@ -110,4 +110,11 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `petdatabase`;
-INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (1, 'HALL, Marshall', '145dfg', '');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (1, 'DOUSIP, Marcus', 'nja86078', '');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (2, 'HOWLETT, Sarah', 'gui89264', 'Hi there');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (3, 'ARULANANTHAN, Alvin', 'njy98623', '');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (4, 'DAVID, Mark', 'apn15385', '');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (5, 'SCHIBECI, Alice', 'afd15591', 'All fixed');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (6, 'STEVENS, Berlinda', 'cjo54843', '');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (7, 'HOWLETT, Sarah', 'gui89264', 'Comment made');
+INSERT INTO `petdatabase`.`physician` (`idphysician`, `physicianName`, `providerNumber`, `physicianComment`) VALUES (8, 'DOUSIP, Marcus', 'nja86078', 'Not again');
