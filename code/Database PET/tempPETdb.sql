cb1d2b5dc1e6d028f909ed498878790ede9878df
Jette McKellar <jette.mckellar@yahoo.com>
2018-05-18 23:48:14 +1000

    Temporary Database PET/tempPETdb.sql to take information from Error Form. Resolves Issue #15
    Signed-off-by: Jette McKellar <jette.mckellar@yahoo.com>

---
+++ code/Database PET/tempPETdb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tempPETdb` DEFAULT CHARACTER SET utf8 ;
+USE `tempPETdb` ;
+
+-- -----------------------------------------------------
+-- Table `tempPETdb`.`errorForm`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tempPETdb`.`errorForm` (
+  `idErrorForm` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `time` TIME NOT NULL,
+  `patientFirstName` VARCHAR(45) NOT NULL,
+  `patientSurname` VARCHAR(45) NOT NULL,
+  `patientId` VARCHAR(15) NOT NULL,
+  `patientType` VARCHAR(45) NOT NULL,
+  `errorType` VARCHAR(45) NOT NULL,
+  `errorComment` VARCHAR(150) NULL,
+  `workerAtFault` VARCHAR(45) NOT NULL,
+  `workerNotified` TINYINT NOT NULL,
+  `location` VARCHAR(45) NOT NULL,
+  `iimsCompleted` TINYINT NOT NULL,
+  `medication` VARCHAR(45) NOT NULL,
+  `severity` VARCHAR(45) NOT NULL,
+  `physicianNotified` TINYINT NOT NULL,
+  `physicianFirstName` VARCHAR(45) NULL,
+  `physicianSurname` VARCHAR(45) NULL,
+  `providerNumber` VARCHAR(45) NULL,
+  `physicianComments` VARCHAR(150) NULL,
+  `dianosis` VARCHAR(150) NULL,
+  PRIMARY KEY (`idErrorForm`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
