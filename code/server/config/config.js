b96462a37984b03758c8248108e376e8a9ddfb93
Beau Johnson <beaujohnson@live.com>
2018-05-27 13:22:50 +1000

    updated the config.js file to point at the servers database instance rather than a local one so that everyone is working with the same data model

--- code/server/config/config.js
+++ code/server/config/config.js
@@ -2,12 +2,12 @@
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    database: process.env.DB_TABLE || 'PETdatabase',
-    user: process.env.DB_USER || 'root',
+    database: process.env.DB_TABLE || 'petdatabase',
+    user: process.env.DB_USER || 'pet_user',
     password: process.env.DB_PASS || 'password',
     options: {
       dialect: 'mysql',
-      host: process.env.HOST || 'localhost',
+      host: process.env.HOST || '18.188.218.170',
       pool: {
         max: 10,
         min: 0,
