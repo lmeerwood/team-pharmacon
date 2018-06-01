4b611a4677178148bf2d308c144164a1508de3b7
Beau Johnson <beaujohnson@live.com>
2018-06-02 09:21:25 +1000

    created 2 new scripts to replace the previous run.sh as we need to have 2 seperate commands for starting Metabase and the Webserver, this will allow us to have 2 terminal windows running and be able to see errors correctly when they occur

---
+++ scripts/metabase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setup Port and Address for Metabase
+export MB_JETTY_PORT=17050;
+export MB_JETTY_HOST=0.0.0.0;
+echo Metabase Server Port and IP Address Set;
+
+# Run Metabase
+java -jar ~/metabase.jar &
+echo Metabase Started;
+
+
