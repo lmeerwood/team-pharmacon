a0684c26cb02b01f8eb3a8c31e60167a8aba8004
Beau Johnson <beaujohnson@live.com>
2018-07-16 18:13:36 +1000

    Added a new route and component for the Search Error functionality that is being added to the application

---
+++ code/client/src/components/searchError.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout row class="pt-5 top-margin">
+    <v-flex xs6 offset-xs3>
+      <section class="page-head">
+        <h1 id="introduction" class="display-1 primary--text">
+          Holding Page For Search Function!!!
+        </h1>
+        <div>
+          <p>This page provides direct links to different sections of the app. It will be removed for production.
+          <br/><br/>Also, none of this layout is permanent. All of it can be changed and modified to suit the end goal. This
+            was simply made to provide a starting ground.</p>
+        </div>
+      </section>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Pharmacy Error Tracker Debug Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.top-margin {
+  margin-top: 40px;
+}
+</style>
