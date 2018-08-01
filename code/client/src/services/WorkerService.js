a2b806b3289853e18c119e663db7d0e1357aed49
Jette McKellar <jette.mckellar@yahoo.com>
2018-08-01 19:59:32 +1000

    Update WorkerService.js to log and retrieve workers.

--- code/client/src/services/WorkerService.js
+++ code/client/src/services/WorkerService.js
@@ -4,4 +4,11 @@ export default {
   getAll () {
     return Api().get('/api/v1/query/worker')
   }
+  logWorker (worker) {
+    return Api().post('/api/v1/query/worker', worker)
+  },
+  getWorker (workerId) {
+    var url = '/api/v1/query/worker/' + workerId
+    return Api().get(url)
+  }
 }
