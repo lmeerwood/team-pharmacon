4e4dc4be3f49f59a8d4c9f6866afe558658954d7
Jette McKellar <jette.mckellar@yahoo.com>
2018-09-08 20:02:23 +1000

    Add UserGuide.pdf to a new folder - resources - on the server and amend Header.vue to have a Help button to download the pdf file. app.js amended to ensure this functionality works

--- code/client/src/components/Header.vue
+++ code/client/src/components/Header.vue
@@ -120,8 +120,8 @@
           </v-list-tile>
         </v-list>
       </v-menu>
+      <v-btn href="http://127.0.0.1:3000/resources/UserGuide.pdf"  @click="message" flat>Help</v-btn>
       <v-btn @click="logout" flat>Logout</v-btn>
-
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -135,13 +135,20 @@ export default {
       authlevel: 'auth'
     })
   },
+  data: () => ({
+    msg: ''
+  }),
   methods: {
     logout: function () {
       this.$store.dispatch('logout')
       this.$router.push('/login')
+    },
+    message: function () {
+      this.msg = 'User Guide has been downloaded'
     }
   }
 }
+
 </script>
 
 <style scoped>
