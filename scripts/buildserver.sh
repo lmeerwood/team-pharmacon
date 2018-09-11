919346c721bb7a6306d3bc73bc2a127d7dd9e4c7
Beau Johnson <beaujohnson@live.com>
2018-09-11 20:05:46 +1000

    Fixed yet another issue - issue with invalid symbol

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -3,56 +3,57 @@
 case $1 in
     start-webserver)
         # Checkout Branch
-        cd ~/pharmacy_app;
-        git checkout master -f;
-        echo Checked out into dev;
+        cd ~/pharmacy_app
+        git checkout master -f
+        echo Checked out into dev
 
         #Start Web Server
-        cd ~/pharmacy_app/code/server;
-        npm start &;
-        wait;
-        echo Web Server Started;
+        cd ~/pharmacy_app/code/server
+        npm start &
+        wait
+        echo Web Server Started
     ;;
     stop-webserver)
         #Kill the node module to stop the web server
-        pkill -f 'node';
-        echo "Web Server has been stopped!";
+        pkill -f 'node'
+        echo "Web Server has been stopped!"
     ;;
 
     start-metabase)
         # Setup Port and Address for Metabase
-        export MB_JETTY_PORT=17050;
-        export MB_JETTY_HOST=0.0.0.0;
-        echo Metabase Server Port and IP Address Set;
+        export MB_JETTY_PORT=17050
+        export MB_JETTY_HOST=0.0.0.0
+        echo Metabase Server Port and IP Address Set
 
         # Run Metabase
-        java -jar ~/metabase.jar &;
-        wait;
-        echo Metabase Started;
+        java -jar ~/metabase.jar &
+        wait
+        echo Metabase Started
     ;;
+
     stop-metabase)
         #Kill the metabase java jar file
-        pkill -f 'java -jar';
-        echo "Metabase has been stopped!";
+        pkill -f 'java -jar'
+        echo "Metabase has been stopped!"
     ;;
 
     build-server)
-        echo "Building and starting The Web Server!";
-        cd ~;
+        echo "Building and starting The Web Server!"
+        cd ~
 
         #creating aliases
-        echo "alias pharmacon='cd ~/pharmacy_app/scripts && ./buildserver.sh'" >> ~/.bashrc;
+        echo "alias pharmacon='cd ~/pharmacy_app/scripts && ./buildserver.sh'" >> ~/.bashrc
 
         #Change into the application folder and checkout appropriate branch
-        cd ~/pharmacy_app;
-        git checkout master -f;
+        cd ~/pharmacy_app
+        git checkout master -f
 
         #Change into the server folder and then npm install all the packages
-        cd ~/pharmacy_app/code/server;
-        npm install;
-        wait;
-        cd ~;
-        echo "The web Server has been built successfully!";
+        cd ~/pharmacy_app/code/server
+        npm install
+        wait
+        cd ~
+        echo "The web Server has been built successfully!"
     ;;
     
     *)
